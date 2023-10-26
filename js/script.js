39836c0ff619228692076a507f318b9013955dfd
Lucas Salvatori <lucas.o.salvatori@gmail.com>
2023-10-26 17:05:42 -0300

    mejoramos enlace de juegos y agregamos animaciones

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+
+function animacion() {
+  // alert("Aaaahhh, queres ver los juegos??")
+  const juegos = document.getElementById("contenedor_juegos")
+  juegos.style.animationName = 'movimiento2'
+  juegos.style.animationDuration = '4s'
+}
