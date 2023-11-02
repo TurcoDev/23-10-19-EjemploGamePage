c4cc8e33c9f27c8f18f89590ecdacf7f447775f4
Lucas Salvatori <lucas.o.salvatori@gmail.com>
2023-11-02 17:27:55 -0300

    Tarjetas de juegos

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ function animacion() {
   juegos.style.animationName = 'movimiento2'
   juegos.style.animationDuration = '4s'
 }
+
+
+// funcionalidad boton ver mas
+function verMas() {
+  let elemento = document.getElementById("texto_juego1");
+  if (elemento.style.height != "fit-content") {
+    elemento.style.height = "fit-content";
+  } else{
+    elemento.style.height = "50px";
+  }
+}
