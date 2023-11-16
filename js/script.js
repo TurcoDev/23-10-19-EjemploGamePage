fad33d24194d2c8d3ecdb4fbde9dc691b95ac4fa
Lucas Salvatori <lucas.o.salvatori@gmail.com>
2023-11-16 16:50:51 -0300

    Vimos event y envio de formulario

--- js/script.js
+++ js/script.js
@@ -9,11 +9,24 @@ function animacion() {
 
 
 // funcionalidad boton ver mas
-function verMas() {
-  let elemento = document.getElementById("texto_juego1");
+function verMas(elemento, event) {
+  // console.log(event);
+  // console.log(event.target);
+  console.log(elemento.parentNode);
+  // console.log(elemento.parentElement);
+  console.log(elemento);
+
+  elemento.parentNode.style.backgroundColor = "rgba(255, 157, 157, 0.5)";
+  // let elem = document.getElementById("texto_juego1");
+  // console.log(elem);
   if (elemento.style.height != "fit-content") {
     elemento.style.height = "fit-content";
   } else{
     elemento.style.height = "50px";
   }
+}
+
+
+function comentario() {
+  alert('Gracias por tu comentario 🎉')
 }
